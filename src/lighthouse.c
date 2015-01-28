05b397d8475563dba5b4fc55661af9b4b4d58f0e
Bram Wasti <bwasti@gmail.com>
2015-01-28 03:05:19 -0500

    clarified license name

--- src/lighthouse.c
+++ src/lighthouse.c
@@ -7,7 +7,8 @@
  *  @section LICENSE
  *
  *  Copyright (c) 2015 Bram Wasti
- *  
+ *  Distributed under the MIT License.
+ *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
