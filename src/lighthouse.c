a9b41fa941879bac09f51d1dc5c1261351c146e9
emgram769 <bwasti@Brams-MacBook.local>
2015-08-29 08:31:43 -0400

    add includes to make 'kill' a valid call on POSIX systems

--- src/lighthouse.c
+++ src/lighthouse.c
@@ -36,8 +36,10 @@
 #include <fcntl.h>
 #include <getopt.h>
 #include <inttypes.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <wordexp.h>
